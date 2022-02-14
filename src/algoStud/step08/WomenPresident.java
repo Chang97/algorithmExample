7f9390d5254e99f83e7a1d1570e639fdf12f4ab1
Chang97 <ckdrbsaos@naver.com>
2022-02-15 00:09:58 +0900

    부녀회장

---
+++ src/algoStud/step08/WomenPresident.java
@@ -0,0 +1,43 @@
+package algoStud.step08;
+
+import java.util.Scanner;
+
+public class WomenPresident {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("층과 호수를 입력하시오.");
+		int t = Integer.parseInt(sc.nextLine()); // 테스트케이스
+		
+		int[] valArr = new int[t];
+		
+		for ( int i = 0; i < t; i++ ) {
+			int k = Integer.parseInt(sc.nextLine()) + 1; // 층
+			int n = Integer.parseInt(sc.nextLine()); // 호수
+			
+			int[][] arr = new int[k][n];
+			for ( int j = 0; j < n; j++ ) {
+				arr[0][j] = j + 1; // 0층 셋
+			}
+			
+			for ( int floor = 1; floor < k; floor++ ) {
+				for ( int num = 0; num < n; num++) {
+					if ( num == 0 ) {
+						arr[floor][num] = 1;
+					} else {
+						arr[floor][num] = arr[floor - 1][num] + arr[floor][num - 1];
+					}
+				}
+			}
+			
+			valArr[i] = arr[k - 1][n - 1];
+		}
+		
+		for (int i = 0; i < t; i++) {
+			System.out.println(valArr[i]);
+		}
+		
+	}
+
+}

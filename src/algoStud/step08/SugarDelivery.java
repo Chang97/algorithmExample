cd678c335b4fba7aa60176040031a0e579f2e38a
Chang97 <ckdrbsaos@naver.com>
2022-02-22 21:34:43 +0900

    임시

---
+++ src/algoStud/step08/SugarDelivery.java
@@ -0,0 +1,22 @@
+package algoStud.step08;
+
+import java.util.Scanner;
+
+public class SugarDelivery {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("키로수 입력 : ");
+		int maxVy = 0;
+		int num = sc.nextInt();
+		
+		if (num >= 3 && num <= 5000) {
+			
+		} else {
+			System.out.println("잘못된 입력");
+		}
+	}
+
+}
+
+
+
